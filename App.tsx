0c07683c9111611c4a6e4aa5b35b0174abb788fe
bart-1 <studio@dziwnykot.pl>
2023-08-18 09:30:29 +0200

    feat: add buttons (dummy) for rows

--- App.tsx
+++ App.tsx
@@ -2,15 +2,16 @@ import * as React from 'react';
 import './style.css';
 
 type ListModel = {
-  grab: string;
+  id: number;
+  grab:string;
   item: string;
   category: string;
   position: number;
 }[];
 
 const listModel: ListModel = [
-  { grab: '...', item: 'rower', category: 'sport', position: 0 },
-  { grab: '...', item: 'chleb', category: 'pieczywo', position: 1 },
+  { id: 0, grab:"", item: 'rower', category: 'sport', position: 2 },
+  { id: 1, grab:"", item: 'chleb', category: 'pieczywo', position: 1 },
 ];
 const sortFn = (arr: ListModel) => {
   return arr.sort((a, b) => a.position - b.position);
@@ -19,29 +20,30 @@ const sortFn = (arr: ListModel) => {
 export default function App() {
   const [moveX, setMoveX] = React.useState(0);
 
-  const tabTitle = Object.keys(listModel[0]).map((element) => (
-    <th>{element}</th>
-  ));
+  const tabTitle = Object.keys(listModel[0]).map((element) => {
+    if (element !== 'id') {
+      return <th>{element}</th>;
+    }
+  });
   const sortedListModel = sortFn(listModel);
 
-  const handleRowMove = (e: React.MouseEvent<HTMLTableRowElement>) => {
-
-    const elementPosition = e.currentTarget.offsetTop
-    if (e.clientX > elementPosition) {
-      setMoveX(prevState => prevState = 1);
-    } else if (e.clientX < elementPosition) {
-      setMoveX(prevState => prevState = -1);
-    }
-    console.log(moveX)
-  };
-  const tabContent = sortedListModel.map((element, index) => (
-    <tr key={'content' + index} onMouseDown={(e) => handleRowMove(e)}>
-      <td style={{userSelect: "none"}}>{element.grab}</td>
-      <td>{element.item}</td>
-      <td>{element.category}</td>
-      <td>{element.position}</td>
-    </tr>
-  ));
+  const handleGrabButton = (e:React.MouseEvent<HTMLButtonElement>, index:number) => {
+if(e.currentTarget.id === "up") {
+console.log("up"+index)
+} else if (e.currentTarget.id) {
+  console.log("down"+index)
+}
+  }
+  const tabContent = sortedListModel.map((element, index) => {
+        return (
+        <tr key={'content' + index}>
+          <td><button onClick={(e) => handleGrabButton(e, index)} id="up">^</button><button onClick={(e)=>handleGrabButton(e, index)} id="down"style={{rotate: 180+"deg"}}>^</button></td>
+          <td>{element.item}</td>
+          <td>{element.category}</td>
+          <td>{element.position}</td>
+        </tr>
+        )
+  });
 
   return (
     <div>
